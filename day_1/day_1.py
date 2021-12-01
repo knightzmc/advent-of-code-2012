dc9182b6f676ce59a888917f92511c5e3fbfe741
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-12-01 09:29:50 +0000

    day 1 part 2

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,14 +1,24 @@
 count = 0
-with open('input.txt', 'r') as lines:
-    prev = None
-    for line in lines.readlines():
-        as_int = int(line)
-        if prev == None:
-            # N / A
-            prev = as_int
-            continue
-        if as_int > prev:
+
+with open('input.txt', 'r') as i:
+    lines = list(map(int, i.readlines()))
+    prev = lines[0]
+    for line in lines[1:]:
+        if line > prev:
             count += 1
-        prev = as_int
+        prev = line
 
 print(count)
+
+
+# part 2
+windowed = zip(lines, lines[1:], lines[2:])
+sums = list(map(sum, windowed))
+prev = sums[0]
+sum_count = 0
+for sum in sums:
+    if sum > prev:
+        sum_count += 1
+    prev = sum
+
+print(sum_count)
